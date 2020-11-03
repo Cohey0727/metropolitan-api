080888395387370ef9bd2bbff6ba9302a01b4dff
Kohei <ohayousagi.ac.kook0727@gmail.com>
2020-11-04 00:28:47 +0900

    develop

--- tickets/app.py
+++ tickets/app.py
@@ -10,10 +10,13 @@ logger.setLevel('INFO')
 table_name = os.environ.get('TABLE_NAME')
 ticket_talble = boto3.resource('dynamodb').Table(table_name)
 
+
 def lambda_handler(event, context):
     project_id = event['queryStringParameters'].get('project_id')
     logger.info(f'TalbeName: {table_name}, projectId: {project_id}')
-    ticket_data = ticket_talble.query(KeyConditionExpression=Key('projectId').eq(project_id))
+    ticket_data = ticket_talble.query(
+        KeyConditionExpression=Key('projectId').eq(project_id)
+    )
 
     return {
         "statusCode": 200,
