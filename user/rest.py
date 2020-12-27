4f1a5c7b0d033b8e50493397cf78daa75ff33c40
Kohei <ohayousagi.ac.kook0727@gmail.com>
2020-12-28 00:41:52 +0900

    develop

--- user/rest.py
+++ user/rest.py
@@ -1,12 +1,10 @@
 from typing import List
 import boto3
 import requests
-import decimal
 import json
 import logging
 import os
-import re
-import uuid
+from datetime import datetime, timedelta
 from boto3.dynamodb.conditions import Key
 from aws_lambda_rest_api import RestApi
 
@@ -20,8 +18,8 @@ user_url = f'https://{AUTH0_DOMAIN}/api/v2/users'
 AUTH0_API_CLIENT_ID = os.environ.get('AUTH0_API_CLIENT_ID')
 AUTH0_API_CLIENT_SECRET = os.environ.get('AUTH0_API_CLIENT_SECRET')
 
-# parameter_table_name = os.environ.get('PARAMETER_TABLE_NAME')
-# parameter_table = boto3.resource('dynamodb').Table(parameter_table_name)
+parameter_table_name = os.environ.get('PARAMETER_TABLE_NAME')
+parameter_table = boto3.resource('dynamodb').Table(parameter_table_name)
 
 
 payload = {
@@ -41,7 +39,8 @@ class UserApi(RestApi):
     }
 
     def list(self, event, context):
-        user_ids: List[str] = event['queryStringParameters']['user_ids']
+        user_ids: List[str] = json.loads(
+            event['queryStringParameters']['user_ids'])
         token = get_access_token()
         headers = {'authorization': f'Bearer {token}'}
         users = []
@@ -56,6 +55,11 @@ class UserApi(RestApi):
 
 
 def get_access_token():
+    params = {'parameterKey': 'Auth0AccessToken'}
+    access_token_data = parameter_table.get_item(Key=params).get('Item')
+    if access_token_data is not None:
+        return access_token_data['value']
+
     headers = {'content-type': 'application/json'}
     url = f'https://{AUTH0_DOMAIN}/oauth/token'
     audience = f'https://{AUTH0_DOMAIN}/api/v2/'
@@ -66,7 +70,13 @@ def get_access_token():
         'grant_type': 'client_credentials',
     }
     res = requests.post(url, headers=headers, data=json.dumps(payload))
-    return json.loads(res.text)['access_token']
+    access_token = json.loads(res.text)['access_token']
+    datetime_now = datetime.now()
+    ttl_time = datetime_now + timedelta(days=1)
+    ttl = str(int(ttl_time.timestamp()))
+    access_token_data = {**params, 'value': access_token, 'ttl': ttl}
+    parameter_table.put_item(Item=access_token_data)
+    return access_token
 
 
 lambda_handler = UserApi().create_handler()
